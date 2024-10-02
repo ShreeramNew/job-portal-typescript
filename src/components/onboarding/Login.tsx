a1f6f7d4c06c14c65170ccf455bb7af714aba62d
ShreeramNew <shreerambca1@gmail.com>
2024-10-02 09:07:31 +0530

    login and signup style changed

--- src/components/onboarding/Login.tsx
+++ src/components/onboarding/Login.tsx
@@ -19,8 +19,8 @@ export default function Login({ isEmployer }: { isEmployer: boolean }) {
    };
 
    return (
-      <div className="w-full h-[100vh] border- border-red-800 flex justify-center items-center bg-gray-100">
-         <div className=" border-blue-900 border-2 w-[26%] h-[60%] rounded-lg shadow-blue-600 shadow-xl bg-gray-200">
+      <div className="w-full h-[100vh] border- border-red-800 flex justify-center items-center">
+         <div className=" border-blue-900 border- w-[93%] md:w-[26%] h-[60%] rounded-lg shadow-blue-600 shadow-xl bg-gradient-to-br from-gray-400 via-gray-200 to-gray-300">
             <Form
                name="basic"
                onFinish={onFinish}
@@ -28,15 +28,16 @@ export default function Login({ isEmployer }: { isEmployer: boolean }) {
                autoComplete="on"
                className=" border- border-green-900 h-full !w-full flex justify-center items-center flex-col gap-[25px] rounded-lg"
             >
-               <h2 className=" text-blue-600 font-bold text-2xl">Login</h2>
+               <h2 className=" text-blue-600 font-bold text-[2rem] md:text-2xl ">Login</h2>
 
                <Form.Item<FieldType>
                   name="email"
                   rules={[{ required: true, message: "Please enter a valid email!" }]}
-                  className="w-[70%] m-0 h-[10%]"
+                  className="w-[83%] md:w-[70%] m-0 h-[10%]"
                >
                   <Input
-                     className=" border-2 border-gray-400 rounded-md px-[10px] py-[2px]  outline-none  h-[35px]"
+                     type="email"
+                     className=" border-b-2 border-blue-900 rounded-md px-[10px] py-[2px] shadow-2xl shadow-blue-900  outline-none h-[40px] md:h-[35px]"
                      placeholder="Enter your email"
                   />
                </Form.Item>
@@ -44,10 +45,10 @@ export default function Login({ isEmployer }: { isEmployer: boolean }) {
                <Form.Item<FieldType>
                   name="password"
                   rules={[{ required: true, message: "Please input your password!" }]}
-                  className="w-[70%] m-0 h-[10%]"
+                  className="w-[83%] md:w-[70%] m-0 h-[10%]"
                >
                   <Input.Password
-                     className=" border-2 border-gray-400 rounded-md px-[10px] py-[2px] outline-none  h-[35px]"
+                     className=" !border-b-2 border-blue-900 rounded-md px-[10px] py-[2px] outline-none h-[40px] md:h-[35px]"
                      placeholder="Enter password"
                   />
                </Form.Item>
@@ -55,12 +56,12 @@ export default function Login({ isEmployer }: { isEmployer: boolean }) {
                <Form.Item className="w-[70%] m-0">
                   <button
                      type="submit"
-                     className=" bg-blue-500 py-[5px] rounded-md w-full text-white "
+                     className=" bg-blue-500 py-[5px] text-xl rounded-md w-full text-white "
                   >
                      Login
                   </button>
                </Form.Item>
-               <p>
+               <p className="text-[1rem]">
                   Don't you have an account?
                   <span
                      className=" text-blue-900 hover:underline cursor-pointer"
